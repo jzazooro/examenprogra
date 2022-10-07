d7e8845d2ba8711267bf3e90b6dfa2662fd3291b
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-07 17:23:32 +0200

    termino los ejercicios

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from ej1.lanzador1 import main1
 from ej2.lanzador2 import main2
-from ej3.lanzador3 import main3
 from ej4.lanzador4 import main4
-from ej5.lanzador5 import main5
 
 if __name__ == '__main__':
     ejercicio = input("Que ejercicio desea hacer: ")
@@ -10,9 +8,5 @@ if __name__ == '__main__':
         main1()
     if ejercicio == 2:
         main2()
-    if ejercicio == 3:
-        main3()
     if ejercicio == 4:
         main4()
-    if ejercicio == 5:
-        main5()
